617242c7202e2d81c7b1058fd3713ff48bd3aeac
Daniel Fernández <xsteguin@gmail.com>
2022-04-27 14:57:27 -0500

    First commit. The PMS5003 sensor is working.

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include "PMS.h"
+
+PMS pms(Serial1);
+PMS::DATA data;
+
+void setup()
+{
+  Serial.begin(9600);   // GPIO1, GPIO3 (TX/RX pin on ESP-12E Development Board)
+  Serial1.begin(9600);  // GPIO2 (D4 pin on ESP-12E Development Board)
+  pms.passiveMode();    // Switch to passive mode
+}
+
+void loop()
+{
+  pms.wakeUp();
+  delay(2000);
+  pms.requestRead();
+
+  if (pms.readUntil(data))
+  {
+    Serial.print("PM 1.0 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_1_0);
+
+    Serial.print("PM 2.5 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_2_5);
+
+    Serial.print("PM 10.0 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_10_0);
+  }
+  else
+  {
+    Serial.println("No data.");
+  }
+  //pms.sleep();
+  //delay(60000);
+}
